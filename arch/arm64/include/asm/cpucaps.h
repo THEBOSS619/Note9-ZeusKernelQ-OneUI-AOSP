22f82c23d588df1051a3cc352b06aa4a905d1ac8
Robin Murphy <robin.murphy@arm.com>
2017-07-25 11:55:42 +0100

    BACKPORT: arm64: Implement pmem API support
    Add a clean-to-point-of-persistence cache maintenance helper, and wire
    up the basic architectural support for the pmem driver based on it.
    
    Reviewed-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Robin Murphy <robin.murphy@arm.com>
    [catalin.marinas@arm.com: move arch_*_pmem() functions to arch/arm64/mm/flush.c]
    [catalin.marinas@arm.com: change dmb(sy) to dmb(osh)]
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Angelo G. Del Regno <kholk11@gmail.com>
    
    Change-Id: I0424902a9dfd71cfbd6d0228d31f71eb57bf5ffe
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- arch/arm64/include/asm/cpucaps.h
+++ arch/arm64/include/asm/cpucaps.h
@@ -38,8 +38,9 @@
 #define ARM64_HARDEN_BRANCH_PREDICTOR		17
 #define ARM64_SSBD				18
 #define ARM64_MISMATCHED_CACHE_TYPE		19
+#define ARM64_HAS_DCPOP				20
 
-#define ARM64_NCAPS				20
-#define ARM64_HAS_CRC32				21
+#define ARM64_NCAPS				21
+#define ARM64_HAS_CRC32				22
 
 #endif /* __ASM_CPUCAPS_H */

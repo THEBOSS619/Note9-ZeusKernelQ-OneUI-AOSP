e60cc8fe37432a85579dfc60bc9da3f0ead3f093
Will Deacon <will.deacon@arm.com>
2017-10-03 19:25:28 +0100

    locking/arch: Remove dummy arch_{read,spin,write}_relax() implementations
    arch_{read,spin,write}_relax() are defined as cpu_relax() by the core
    code, so architectures that can't do better (i.e. most of them) don't
    need to bother with the dummy definitions.
    
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: paulmck@linux.vnet.ibm.com
    Link: http://lkml.kernel.org/r/1507055129-12300-3-git-send-email-will.deacon@arm.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- arch/arm64/include/asm/spinlock.h
+++ arch/arm64/include/asm/spinlock.h
@@ -310,10 +310,6 @@ static inline int arch_read_trylock(arch_rwlock_t *rw)
 #define arch_read_lock_flags(lock, flags) arch_read_lock(lock)
 #define arch_write_lock_flags(lock, flags) arch_write_lock(lock)
 
-#define arch_spin_relax(lock)	cpu_relax()
-#define arch_read_relax(lock)	cpu_relax()
-#define arch_write_relax(lock)	cpu_relax()
-
 /*
  * Accesses appearing in program order before a spin_lock() operation
  * can be reordered with accesses inside the critical section, by virtue

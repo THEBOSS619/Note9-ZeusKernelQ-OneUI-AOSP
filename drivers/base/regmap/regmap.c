35d8bb8235d349f001d17252ac77bf8579803f1e
Mark Brown <broonie@kernel.org>
2017-11-03 19:50:20 +0000

    regmap: Also protect hwspinlock in error handling path
    The previous patch to allow the hwspinlock code to be disabled missed
    handling the free in the error path, do so using the better IS_ENABLED()
    pattern as suggested by Baolin. While we're at it also check that we have
    a hardware spinlock before freeing it - the core code reports an error
    when freeing an invalid lock.
    
    Suggested-by: Baolin Wang <baolin.wang@linaro.org>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/base/regmap/regmap.c
+++ drivers/base/regmap/regmap.c
@@ -1118,7 +1118,8 @@ err_range:
 	regmap_range_exit(map);
 	kfree(map->work_buf);
 err_hwlock:
-	hwspin_lock_free(map->hwlock);
+	if (IS_ENABLED(REGMAP_HWSPINLOCK) && map->hwlock)
+		hwspin_lock_free(map->hwlock);
 err_map:
 	kfree(map);
 err:

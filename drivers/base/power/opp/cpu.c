4147813403a9e379b293429334a67a8b586c8aa0
Jia-Ju Bai <baijiaju1990@gmail.com>
2018-01-26 16:48:49 +0800

    opp: cpu: Replace GFP_ATOMIC with GFP_KERNEL in dev_pm_opp_init_cpufreq_table
    After checking all possible call chains to
    dev_pm_opp_init_cpufreq_table() here,
    my tool finds that this function is never called in atomic context,
    namely never in an interrupt handler or holding a spinlock.
    And dev_pm_opp_init_cpufreq_table() calls dev_pm_opp_get_opp_count(),
    which calls mutex_lock that can sleep.
    It indicates that atmtcp_v_send() can call functions which may sleep.
    Thus GFP_ATOMIC is not necessary, and it can be replaced with GFP_KERNEL.
    
    This is found by a static analysis tool named DCNS written by myself.
    
    Signed-off-by: Jia-Ju Bai <baijiaju1990@gmail.com>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- drivers/base/power/opp/cpu.c
+++ drivers/base/power/opp/cpu.c
@@ -64,7 +64,7 @@ int dev_pm_opp_init_cpufreq_table(struct device *dev,
 		goto out;
 	}
 
-	freq_table = kcalloc((max_opps + 1), sizeof(*freq_table), GFP_ATOMIC);
+	freq_table = kcalloc((max_opps + 1), sizeof(*freq_table), GFP_KERNEL);
 	if (!freq_table) {
 		ret = -ENOMEM;
 		goto out;

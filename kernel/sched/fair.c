7dac12722d5aa0db5165db64447e350d426a859d
Pavankumar Kondeti <pkondeti@codeaurora.org>
2018-02-08 16:43:45 +0530

    sched/fair: fix incorrect CPU selection for non latency sensitive tasks
    The Non latency sensitive tasks CPU selection targets for an active
    CPU in the little cluster. The shallowest c-state CPU is stored as
    a backup. However if all CPUs in the little cluster are idle, we pick
    an active CPU in the BIG cluster as the target CPU. This incorrect
    choice of the target CPU may not get corrected by the
    select_energy_cpu_idx() depending on the energy difference between
    previous CPU and target CPU.
    
    This can be fixed easily by maintaining the same variable that tracks
    maximum capacity of the traversed CPU for both idle and active CPUs.
    
    Change-Id: I3efb8bc82ff005383163921ef2bd39fcac4589ad
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: joshuous <joshuous@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6894,7 +6894,6 @@ static int start_cpu(bool boosted)
 static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 				   bool boosted, bool prefer_idle)
 {
-	unsigned long best_idle_min_cap_orig = ULONG_MAX;
 	unsigned long min_util = boosted_task_util(p);
 	unsigned long target_capacity = ULONG_MAX;
 	unsigned long min_wake_util = ULONG_MAX;
@@ -7075,6 +7074,13 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 				continue;
 
 			/*
+			 * Favor CPUs with smaller capacity for Non latency
+			 * sensitive tasks.
+			 */
+			if (capacity_orig > target_capacity)
+				continue;
+
+			/*
 			 * Case B) Non latency sensitive tasks on IDLE CPUs.
 			 *
 			 * Find an optimal backup IDLE CPU for non latency
@@ -7101,10 +7107,6 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			if (idle_cpu(i)) {
 				int idle_idx = idle_get_state_idx(cpu_rq(i));
 
-				/* Select idle CPU with lower cap_orig */
-				if (capacity_orig > best_idle_min_cap_orig)
-					continue;
-
 				/*
 				 * Skip CPUs in deeper idle state, but only
 				 * if they are also less energy efficient.
@@ -7117,7 +7119,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 					continue;
 
 				/* Keep track of best idle CPU */
-				best_idle_min_cap_orig = capacity_orig;
+				target_capacity = capacity_orig;
 				best_idle_cstate = idle_idx;
 				best_idle_cpu = i;
 				continue;
@@ -7142,10 +7144,6 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			 * smallest maximum capacity and highest spare maximum
 			 * capacity.
 			 */
-
-			/* Favor CPUs with smaller capacity */
-			if (capacity_orig > target_capacity)
-				continue;
 
 			/* Favor CPUs with maximum spare capacity */
 			if ((capacity_orig == target_capacity) &&

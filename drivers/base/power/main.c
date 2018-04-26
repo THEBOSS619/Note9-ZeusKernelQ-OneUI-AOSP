cb2ef9f2a4901db55d15d059a10802ad3aff0eb5
Bjorn Helgaas <bhelgaas@google.com>
2018-04-26 16:36:27 -0500

    PM / core: Remove unused initcall_debug_report() arguments
    Commit e8bca479c3f2 (PM / sleep: trace events for device PM callbacks)
    removed the only uses of "state" and "info" from initcall_debug_report().
    
    Remove the now-unused arguments completely.
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -221,8 +221,7 @@ static ktime_t initcall_debug_start(struct device *dev)
 }
 
 static void initcall_debug_report(struct device *dev, ktime_t calltime,
-				  int error, pm_message_t state,
-				  const char *info)
+				  int error)
 {
 	ktime_t rettime;
 	s64 nsecs;
@@ -435,9 +434,9 @@ static int dpm_run_callback(pm_callback_t cb, struct device *dev,
 	trace_device_pm_callback_end(dev, error);
 	suspend_report_result(cb, error);
 
-	initcall_debug_report(dev, calltime, error, state, info);
+	initcall_debug_report(dev, calltime, error);
 #if defined(CONFIG_SEC_NAD_BALANCER) && defined(CONFIG_SEC_FACTORY)
-	sec_nad_debug_report(dev, nad_calltime, state);
+	sec_nad_debug_report(dev, nad_calltime, error);
 #endif
 
 	return error;
@@ -1418,7 +1417,7 @@ static int legacy_suspend(struct device *dev, pm_message_t state,
 	trace_device_pm_callback_end(dev, error);
 	suspend_report_result(cb, error);
 
-	initcall_debug_report(dev, calltime, error, state, info);
+	initcall_debug_report(dev, calltime, error);
 
 	return error;
 }

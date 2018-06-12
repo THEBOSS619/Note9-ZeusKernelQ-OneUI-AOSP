51cca0611d422fad402592cedbd503d222a37cea
Johnlay Park <jonglae.park@samsung.com>
2018-06-12 20:03:25 +0900

    sched: frt: Add the rt_rq load update.
    Change-Id: I676ecfa2aec75c46144f78fc90981ff43c8833c0
    Signed-off-by: Johnlay Park <jonglae.park@samsung.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8726,7 +8726,7 @@ static void update_blocked_averages(int cpu)
 		if (se && !skip_blocked_update(se))
 			update_load_avg(se, 0);
 	}
-
+	update_rt_rq_load_avg(rq_clock_task(rq), cpu, &rq->rt, 0);
 	raw_spin_unlock_irqrestore(&rq->lock, flags);
 }
 
@@ -8786,6 +8786,7 @@ static inline void update_blocked_averages(int cpu)
 	raw_spin_lock_irqsave(&rq->lock, flags);
 	update_rq_clock(rq);
 	update_cfs_rq_load_avg(cfs_rq_clock_task(cfs_rq), cfs_rq, true);
+	update_rt_rq_load_avg(rq_clock_task(rq), cpu, &rq->rt, 0);
 	raw_spin_unlock_irqrestore(&rq->lock, flags);
 }
 

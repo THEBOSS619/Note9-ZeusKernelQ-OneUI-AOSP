fad32ddad4fb54efef6f4e6f742c07cef4cb513b
joshuous <joshuous@gmail.com>
2018-07-10 12:22:59 +0200

    sched/stune: Rename stune_boost() to do_stune_sched_boost()
    To reflect that the function is to be used mainly with CAF's devices
    that have sched_boost. However, developers may use it as a switch to
    dynamically boost schedtune to the values specified in
    /dev/stune/*/schedtune.sched_boost.
    
    Signed-off-by: joshuous <joshuous@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -4128,7 +4128,7 @@ static inline void rseq_syscall(struct pt_regs *regs)
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 int do_stune_boost(char *st_name, int boost);
+int do_stune_sched_boost(char *st_name);
 int reset_stune_boost(char *st_name);
-int stune_boost(char *st_name);
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 #endif

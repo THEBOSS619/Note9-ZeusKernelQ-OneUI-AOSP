c9815c27f110ddd79a8dd09bf714df134491ec9f
Kyle Lin <kylelin@google.com>
2019-03-20 00:00:28 +0800

    BACKPORT: cpufreq: stats: replace the global lock with atomic
    We want to reduce the lock contention so replace the global lock with
    atomic.
    
    bug: 127722781
    Change-Id: I08ed3d55bf6bf17f31f4017c82c998fb513bad3e
    Signed-off-by: Kyle Lin <kylelin@google.com>

--- drivers/cpufreq/cpufreq_stats.c
+++ drivers/cpufreq/cpufreq_stats.c
@@ -14,15 +14,13 @@
 #include <linux/module.h>
 #include <linux/slab.h>
 
-static DEFINE_SPINLOCK(cpufreq_stats_lock);
-
 struct cpufreq_stats {
 	unsigned int total_trans;
-	unsigned long long last_time;
+	atomic64_t last_time;
 	unsigned int max_state;
 	unsigned int state_num;
 	unsigned int last_index;
-	u64 *time_in_state;
+	atomic64_t *time_in_state;
 	unsigned int *freq_table;
 #ifdef CONFIG_CPU_FREQ_STAT_DETAILS
 	unsigned int *trans_table;
@@ -32,12 +30,10 @@ struct cpufreq_stats {
 static void cpufreq_stats_update(struct cpufreq_stats *stats)
 {
 	unsigned long long cur_time = get_jiffies_64();
-	unsigned long flags;
+	unsigned long long time = cur_time;
 
-	spin_lock_irqsave(&cpufreq_stats_lock, flags);
-	stats->time_in_state[stats->last_index] += cur_time - stats->last_time;
-	stats->last_time = cur_time;
-	spin_unlock_irqrestore(&cpufreq_stats_lock, flags);
+	time = atomic64_xchg(&stats->last_time, time);
+	atomic64_add(cur_time - time, &stats->time_in_state[stats->last_index]);
 }
 
 static ssize_t show_total_trans(struct cpufreq_policy *policy, char *buf)
@@ -55,7 +51,8 @@ static ssize_t show_time_in_state(struct cpufreq_policy *policy, char *buf)
 	for (i = 0; i < stats->state_num; i++) {
 		len += sprintf(buf + len, "%u %llu\n", stats->freq_table[i],
 			(unsigned long long)
-			jiffies_64_to_clock_t(stats->time_in_state[i]));
+			jiffies_64_to_clock_t(atomic64_read(
+					&stats->time_in_state[i])));
 	}
 	return len;
 }
@@ -172,7 +169,7 @@ void cpufreq_stats_create_table(struct cpufreq_policy *policy)
 	cpufreq_for_each_valid_entry(pos, table)
 		count++;
 
-	alloc_size = count * sizeof(int) + count * sizeof(u64);
+	alloc_size = count * sizeof(int) + count * sizeof(atomic64_t);
 
 #ifdef CONFIG_CPU_FREQ_STAT_DETAILS
 	alloc_size += count * count * sizeof(int);
@@ -197,7 +194,7 @@ void cpufreq_stats_create_table(struct cpufreq_policy *policy)
 			stats->freq_table[i++] = pos->frequency;
 
 	stats->state_num = i;
-	stats->last_time = get_jiffies_64();
+	atomic64_set(&stats->last_time, get_jiffies_64());
 	stats->last_index = freq_table_get_index(stats, policy->cur);
 
 	policy->stats = stats;

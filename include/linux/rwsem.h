e292734c6e3dba1ba6b5fd5e4beb9803ae54cf32
Waiman Long <longman@redhat.com>
2019-04-04 13:43:20 -0400

    locking/rwsem: Optimize rwsem structure for uncontended lock acquisition
    For an uncontended rwsem, count and owner are the only fields a task
    needs to touch when acquiring the rwsem. So they are put next to each
    other to increase the chance that they will share the same cacheline.
    
    On a ThunderX2 99xx (arm64) system with 32K L1 cache and 256K L2
    cache, a rwsem locking microbenchmark with one locking thread was
    run to write-lock and write-unlock an array of rwsems separated 2
    cachelines apart in a 1M byte memory block. The locking rates (kops/s)
    of the microbenchmark when the rwsems are at various "long" (8-byte)
    offsets from beginning of the cacheline before and after the patch were
    as follows:
    
      Cacheline Offset   Pre-patch    Post-patch
      ----------------   ---------    ----------
            0             17,449        16,588
            1             17,450        16,465
    	2             17,450        16,460
    	3             17,453        16,462
    	4             14,867        16,471
    	5             14,867        16,470
    	6             14,853        16,464
    	7             14,867        13,172
    
    Before the patch, the count and owner are 4 "long"s apart. After the
    patch, they are only 1 "long" apart.
    
    The rwsem data have to be loaded from the L3 cache for each access. It
    can be seen that the locking rates are more consistent after the patch
    than before. Note that for this particular system, the performance
    drop happens whenever the count and owner are at an odd multiples of
    "long"s apart. No performance drop was observed when only a single rwsem
    was used (hot cache). So the drop is likely just an idiosyncrasy of the
    cache architecture of this chip than an inherent problem with the patch.
    
    Suggested-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Waiman Long <longman@redhat.com>
    Acked-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Davidlohr Bueso <dave@stgolabs.net>
    Cc: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Tim Chen <tim.c.chen@linux.intel.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Link: http://lkml.kernel.org/r/20190404174320.22416-12-longman@redhat.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- include/linux/rwsem.h
+++ include/linux/rwsem.h
@@ -19,25 +19,35 @@
 #include <linux/osq_lock.h>
 #endif
 
-struct rw_semaphore;
 
 #ifdef CONFIG_RWSEM_GENERIC_SPINLOCK
 #include <linux/rwsem-spinlock.h> /* use a generic implementation */
 #define __RWSEM_INIT_COUNT(name)	.count = RWSEM_UNLOCKED_VALUE
 #else
-/* All arch specific implementations share the same struct */
+/*
+ * For an uncontended rwsem, count and owner are the only fields a task
+ * needs to touch when acquiring the rwsem. So they are put next to each
+ * other to increase the chance that they will share the same cacheline.
+ *
+ * In a contended rwsem, the owner is likely the most frequently accessed
+ * field in the structure as the optimistic waiter that holds the osq lock
+ * will spin on owner. For an embedded rwsem, other hot fields in the
+ * containing structure should be moved further away from the rwsem to
+ * reduce the chance that they will share the same cacheline causing
+ * cacheline bouncing problem.
+ */
 struct rw_semaphore {
 	atomic_long_t count;
-	struct list_head wait_list;
-	raw_spinlock_t wait_lock;
 #ifdef CONFIG_RWSEM_SPIN_ON_OWNER
-	struct optimistic_spin_queue osq; /* spinner MCS lock */
 	/*
 	 * Write owner. Used as a speculative check to see
 	 * if the owner is running on the cpu.
 	 */
 	struct task_struct *owner;
+	struct optimistic_spin_queue osq; /* spinner MCS lock */
 #endif
+	raw_spinlock_t wait_lock;
+	struct list_head wait_list;
 #ifdef CONFIG_DEBUG_LOCK_ALLOC
 	struct lockdep_map	dep_map;
 #endif

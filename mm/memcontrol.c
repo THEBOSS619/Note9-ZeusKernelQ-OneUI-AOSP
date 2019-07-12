5c95ded6292fe997fd5caa188f92c4b8c269a49a
Shakeel Butt <shakeelb@google.com>
2019-07-11 20:55:48 -0700

    memcg, oom: no oom-kill for __GFP_RETRY_MAYFAIL
    The documentation of __GFP_RETRY_MAYFAIL clearly mentioned that the OOM
    killer will not be triggered and indeed the page alloc does not invoke OOM
    killer for such allocations.  However we do trigger memcg OOM killer for
    __GFP_RETRY_MAYFAIL.  Fix that.  This flag will used later to not trigger
    oom-killer in the charging path for fanotify and inotify event
    allocations.
    
    Link: http://lkml.kernel.org/r/20190514212259.156585-1-shakeelb@google.com
    Signed-off-by: Shakeel Butt <shakeelb@google.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Cc: Amir Goldstein <amir73il@gmail.com>
    Cc: Jan Kara <jack@suse.cz>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Roman Gushchin <guro@fb.com>
    Cc: Vladimir Davydov <vdavydov.dev@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -1946,7 +1946,6 @@ static int try_charge(struct mem_cgroup *memcg, gfp_t gfp_mask,
 	unsigned long nr_reclaimed;
 	bool may_swap = true;
 	bool drained = false;
-	bool oomed = false;
 	enum oom_status oom_status;
 
 	if (mem_cgroup_is_root(memcg))
@@ -2035,7 +2034,7 @@ retry:
 	if (nr_retries--)
 		goto retry;
 
-	if (gfp_mask & __GFP_RETRY_MAYFAIL && oomed)
+	if (gfp_mask & __GFP_RETRY_MAYFAIL)
 		goto nomem;
 
 	if (gfp_mask & __GFP_NOFAIL)
@@ -2056,7 +2055,6 @@ retry:
 	switch (oom_status) {
 	case OOM_SUCCESS:
 		nr_retries = MEM_CGROUP_RECLAIM_RETRIES;
-		oomed = true;
 		goto retry;
 	case OOM_FAILED:
 		goto force;

e0d6252ce4d61891786a846439d7fe856010ac75
Vincent Guittot <vincent.guittot@linaro.org>
2020-02-24 09:52:14 +0000

    [BACKPORT]sched/fair: Reorder enqueue/dequeue_task_fair path
    The walk through the cgroup hierarchy during the enqueue/dequeue of a task
    is split in 2 distinct parts for throttled cfs_rq without any added value
    but making code less readable.
    
    Change the code ordering such that everything related to a cfs_rq
    (throttled or not) will be done in the same loop.
    
    In addition, the same steps ordering is used when updating a cfs_rq:
    
     - update_load_avg
     - update_cfs_group
     - update *h_nr_running
    
    This reordering enables the use of h_nr_running in PELT algorithm.
    
    No functional and performance changes are expected and have been noticed
    during tests.
    
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: "Dietmar Eggemann <dietmar.eggemann@arm.com>"
    Acked-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Valentin Schneider <valentin.schneider@arm.com>
    Cc: Phil Auld <pauld@redhat.com>
    Cc: Hillf Danton <hdanton@sina.com>
    Link: https://lore.kernel.org/r/20200224095223.13361-5-mgorman@techsingularity.net
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5558,32 +5558,31 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 		cfs_rq = cfs_rq_of(se);
 		enqueue_entity(cfs_rq, se, flags);
 
-		/*
-		 * end evaluation on encountering a throttled cfs_rq
-		 *
-		 * note: in the case of encountering a throttled cfs_rq we will
-		 * post the final h_nr_running increment below.
-		 */
-		if (cfs_rq_throttled(cfs_rq))
-			break;
 		cfs_rq->h_nr_running++;
 		walt_inc_cfs_cumulative_runnable_avg(cfs_rq, p);
+
+		/* end evaluation on encountering a throttled cfs_rq */
+		if (cfs_rq_throttled(cfs_rq))
+			goto enqueue_throttle;
 
 		flags = ENQUEUE_WAKEUP;
 	}
 
 	for_each_sched_entity(se) {
 		cfs_rq = cfs_rq_of(se);
-		cfs_rq->h_nr_running++;
 		walt_inc_cfs_cumulative_runnable_avg(cfs_rq, p);
 
+		/* end evaluation on encountering a throttled cfs_rq */
 		if (cfs_rq_throttled(cfs_rq))
-			break;
+			goto enqueue_throttle;
 
 		update_load_avg(cfs_rq, se, UPDATE_TG);
 		update_cfs_shares(se);
+
+		cfs_rq->h_nr_running++;
 	}
 
+enqueue_throttle:
 	if (!se)
 		add_nr_running(rq, 1);
 
@@ -5670,16 +5669,12 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 		cfs_rq = cfs_rq_of(se);
 		dequeue_entity(cfs_rq, se, flags);
 
-		/*
-		 * end evaluation on encountering a throttled cfs_rq
-		 *
-		 * note: in the case of encountering a throttled cfs_rq we will
-		 * post the final h_nr_running decrement below.
-		*/
-		if (cfs_rq_throttled(cfs_rq))
-			break;
 		cfs_rq->h_nr_running--;
 		walt_dec_cfs_cumulative_runnable_avg(cfs_rq, p);
+
+		/* end evaluation on encountering a throttled cfs_rq */
+		if (cfs_rq_throttled(cfs_rq))
+			goto dequeue_throttle;
 
 		/* Don't dequeue parent if it has other entities besides us */
 		if (cfs_rq->load.weight) {
@@ -5700,11 +5695,11 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 		int update_flags;
 
 		cfs_rq = cfs_rq_of(se);
-		cfs_rq->h_nr_running--;
 		walt_dec_cfs_cumulative_runnable_avg(cfs_rq, p);
 
+		/* end evaluation on encountering a throttled cfs_rq */
 		if (cfs_rq_throttled(cfs_rq))
-			break;
+			goto dequeue_throttle;
 
 		update_flags = UPDATE_TG;
 
@@ -5713,8 +5708,11 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 
 		update_load_avg(cfs_rq, se, update_flags);
 		update_cfs_shares(se);
+
+		cfs_rq->h_nr_running--;
 	}
 
+dequeue_throttle:
 	if (!se)
 		sub_nr_running(rq, 1);
 

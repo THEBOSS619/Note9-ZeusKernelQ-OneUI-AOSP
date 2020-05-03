4b4fc90f42e1d4b980a5b0452101c505b9d73925
THEBOSS <ehabhani59@gmail.com>
2020-05-03 23:39:21 +0300

    sdcardfs: Clean Sammy's Dirty Work...
    We will reapply related commits later

--- fs/sdcardfs/inode.c
+++ fs/sdcardfs/inode.c
@@ -20,6 +20,7 @@
 
 #include "sdcardfs.h"
 #include <linux/fs_struct.h>
+#include <linux/ratelimit.h>
 
 const struct cred *override_fsids(struct sdcardfs_sb_info *sbi,
 		struct sdcardfs_inode_data *data)
@@ -557,13 +558,10 @@ static int sdcardfs_permission(struct vfsmount *mnt, struct inode *inode, int ma
 	struct inode tmp;
 	struct sdcardfs_inode_data *top = top_data_get(SDCARDFS_I(inode));
 
+	if (IS_ERR(mnt))
+		return PTR_ERR(mnt);
 	if (!top)
 		return -EINVAL;
-		
-	if (IS_ERR(mnt)) {
-		data_put(top);
-		return PTR_ERR(mnt);
-	}
 
 	/*
 	 * Permission check on sdcardfs inode.

175f91b52d01adbd38e2ebaa0ac987a1a0af84ea
Paul E. McKenney <paulmck@kernel.org>
2020-05-28 08:49:29 -0700

    lockdep: Complain only once about RCU in extended quiescent state
    Currently, lockdep_rcu_suspicious() complains twice about RCU read-side
    critical sections being invoked from within extended quiescent states,
    for example:
    
    	RCU used illegally from idle CPU!
    	rcu_scheduler_active = 2, debug_locks = 1
    	RCU used illegally from extended quiescent state!
    
    This commit therefore saves a couple lines of code and one line of
    console-log output by eliminating the first of these two complaints.
    
    Link: https://lore.kernel.org/lkml/87wo4wnpzb.fsf@nanos.tec.linutronix.de
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/locking/lockdep.c
+++ kernel/locking/lockdep.c
@@ -4508,9 +4508,7 @@ void lockdep_rcu_suspicious(const char *file, const int line, const char *s)
 	printk("\n%srcu_scheduler_active = %d, debug_locks = %d\n",
 	       !rcu_lockdep_current_cpu_online()
 			? "RCU used illegally from offline CPU!\n"
-			: !rcu_is_watching()
-				? "RCU used illegally from idle CPU!\n"
-				: "",
+			: "",
 	       rcu_scheduler_active, debug_locks);
 
 	/*

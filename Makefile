THEBOSS619/Note9-Zeus-Q10.0

Let's declare what Zeus Kernel are using...
I mean.. why not ? ;)

Makefile
@@ -1,7 +1,7 @@
 VERSION = 4
 PATCHLEVEL = 9
 SUBLEVEL = 214
-EXTRAVERSION =
+EXTRAVERSION = -GCMA-GCC10-DragonTC.CLANG.9.0.5
 NAME = Roaring Lionus
 
 # *DOCUMENTATION*

THEBOSS619/Note9-ZeusKernelQ-OneUI-AOSP

Makefile
@@ -1,7 +1,7 @@
 VERSION = 4
 PATCHLEVEL = 9
 SUBLEVEL = 227
-EXTRAVERSION =
+EXTRAVERSION = -EMS-GCC11-DragonTC.CLANG.9.0.5
 NAME = Roaring Lionus
 
 # *DOCUMENTATION*
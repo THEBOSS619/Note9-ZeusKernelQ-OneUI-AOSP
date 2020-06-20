bb9d5929f5de6c35bb1cecd30b89ee7d0f5294a8
THEBOSS <ehabhani59@gmail.com>
2020-06-20 23:12:40 +0300

    power/suspend: Remove unused line

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -44,7 +44,6 @@ const char * const mem_sleep_labels[] = {
 };
 
 suspend_state_t mem_sleep_current = PM_SUSPEND_FREEZE;
-static suspend_state_t mem_sleep_default = PM_SUSPEND_MEM;
 
 suspend_state_t pm_suspend_target_state;
 EXPORT_SYMBOL_GPL(pm_suspend_target_state);

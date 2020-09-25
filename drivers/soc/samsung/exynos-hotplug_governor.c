057557c8e2da286a052fbe82aad4f05b081d79df
THEBOSS619 <ehabhani59@gmail.com>
2020-09-25 21:01:18 +0300

    exynoshp: prevent preemption by lowering sched_priority
    the MIN_NICE got us covered + this should make sure it have higher
    priority than other/idle tasks

--- drivers/soc/samsung/exynos-hotplug_governor.c
+++ drivers/soc/samsung/exynos-hotplug_governor.c
@@ -451,7 +451,7 @@ static void __exynos_hpgov_set_disable(void)
 
 static void __exynos_hpgov_set_enable(void)
 {
-	struct sched_param param = { .sched_priority = 20 };
+	struct sched_param param = { .sched_priority = 4 };
 
 	exynos_hpgov.mode = QUAD;
 	exynos_hpgov.user_mode = DISABLE;
@@ -470,7 +470,7 @@ static void __exynos_hpgov_set_enable(void)
 	}
 
 	set_user_nice(exynos_hpgov.task, MIN_NICE);
-	sched_setscheduler_nocheck(exynos_hpgov.task, SCHED_FIFO, &param);
+	sched_setscheduler_nocheck(exynos_hpgov.task, SCHED_NORMAL, &param);
 	set_cpus_allowed_ptr(exynos_hpgov.task, cpu_coregroup_mask(0));
 
 	if (exynos_hpgov.task)

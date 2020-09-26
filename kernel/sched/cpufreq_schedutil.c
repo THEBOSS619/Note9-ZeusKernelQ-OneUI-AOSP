e5bc8d03796b30bf2f42a63d6a3fccb86849305d
THEBOSS619 <ehabhani59@gmail.com>
2020-09-26 04:59:33 +0300

    schedutil: remove checking the current processor number
    since now schedutil can detect what core/cpu got hotplugged out/in on
    real-time sessions without needing to call out smp_processor_id()

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -389,7 +389,6 @@ static inline bool use_pelt(void)
 
 static void sugov_get_util(unsigned long *util, unsigned long *max, u64 time, int cpu)
 {
-	int cpu = smp_processor_id();
 	struct rq *rq = cpu_rq(cpu);
 	unsigned long max_cap, rt;
 	s64 delta;

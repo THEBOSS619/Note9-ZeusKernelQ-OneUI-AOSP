unknownbaka/utopia_kernel_polaris

sched/fair: Force CPU capacity update from higher level sched domain
A CPU capacity is updated in update_cpu_capacity(). This is called
when the CPU is doing load balance in the lowest level sched domain.
The load balance is disabled for the lowest level sched domain, when
there is only 1 CPU in the higher level domain. When there is only
1 CPU in the higher capacity cluster, the CPU capacity and the
root domain's max_cpu_capacity are not getting updated correctly.

Due to the above mentioned problem, a task running on the lower
capacity CPU never upmigrates to the higher capacity CPU thinking
it is running on the max capacity CPU. Fix this by forcing the
CPU capacity update from a higher level sched domain if it has
only 1 CPU.

Change-Id: Iddb900962ab988ffbd08bc92348bd491fa2002ad
Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

kernel/sched/fair.c
@@ -8789,9 +8789,11 @@ skip_unlock: __attribute__ ((unused));
 		capacity = 1;
 
 	cpu_rq(cpu)->cpu_capacity = capacity;
-	sdg->sgc->capacity = capacity;
-	sdg->sgc->max_capacity = capacity;
-	sdg->sgc->min_capacity = capacity;
+	if (!sd->child) {
+		sdg->sgc->capacity = capacity;
+		sdg->sgc->max_capacity = capacity;
+		sdg->sgc->min_capacity = capacity;
+	}
 }
 
 void update_group_capacity(struct sched_domain *sd, int cpu)
@@ -8805,9 +8807,16 @@ void update_group_capacity(struct sched_domain *sd, int cpu)
 	interval = clamp(interval, 1UL, max_load_balance_interval);
 	sdg->sgc->next_update = jiffies + interval;
 
-	if (!child) {
+	/*
+	 * When there is only 1 CPU in the sched group of a higher
+	 * level sched domain (sd->child != NULL), the load balance
+	 * does not happen for the last level sched domain. Check
+	 * this condition and update the CPU capacity accordingly.
+	 */
+	if (cpumask_weight(sched_group_cpus(sdg)) == 1) {
 		update_cpu_capacity(sd, cpu);
-		return;
+		if (!child)
+			return;
 	}
 
 	capacity = 0;

THEBOSS619/Note9-ZeusKernelQ-OneUI-AOSP

Signed-off-by: celtare21 <celtare21@gmail.com>

kernel/sched/fair.c
@@ -9425,9 +9425,11 @@ skip_unlock: __attribute__ ((unused));
 	update_lbt_overutil(cpu, capacity);
 
 	cpu_rq(cpu)->cpu_capacity = capacity;
-	sdg->sgc->capacity = capacity;
-	sdg->sgc->max_capacity = capacity;
-	sdg->sgc->min_capacity = capacity;
+	if (!sd->child) {
+		sdg->sgc->capacity = capacity;
+		sdg->sgc->max_capacity = capacity;
+		sdg->sgc->min_capacity = capacity;
+	}
 }
 
 void update_group_capacity(struct sched_domain *sd, int cpu)
@@ -9441,9 +9443,16 @@ void update_group_capacity(struct sched_domain *sd, int cpu)
 	interval = clamp(interval, 1UL, max_load_balance_interval);
 	sdg->sgc->next_update = jiffies + interval;
 
-	if (!child) {
+	/*
+	 * When there is only 1 CPU in the sched group of a higher
+	 * level sched domain (sd->child != NULL), the load balance
+	 * does not happen for the last level sched domain. Check
+	 * this condition and update the CPU capacity accordingly.
+	 */
+	if (cpumask_weight(sched_group_span(sdg)) == 1) {
 		update_cpu_capacity(sd, cpu);
-		return;
+		if (!child)
+			return;
 	}
 
 	capacity = 0;